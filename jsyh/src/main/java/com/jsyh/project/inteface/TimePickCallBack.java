9bc1d4d957244d40576ac8cc909490d123a037d0
Masson_company <1003682479@qq.com>
2021-03-24 16:56:45 +0800

    second commit

---
+++ jsyh/src/main/java/com/jsyh/project/inteface/TimePickCallBack.java
@@ -0,0 +1,7 @@
+package com.jsyh.project.inteface;
+
+public interface TimePickCallBack {
+
+    void back(String data);
+
+}
--- jsyh/src/main/java/com/jsyh/project/utils/MyUtils.java
+++ jsyh/src/main/java/com/jsyh/project/utils/MyUtils.java
@@ -1,9 +1,28 @@
 package com.jsyh.project.utils;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.TimePickerView;
+import com.jsyh.project.R;
+import com.jsyh.project.inteface.TimePickCallBack;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public enum MyUtils {
     INSTANCE;
@@ -38,8 +57,8 @@ public enum MyUtils {
         return false;
     }
 
-    public static void e(String logFilter, String logValue) {
-        Log.e(logFilter, logValue);
+    public static void e(String logValue) {
+        Log.e(TAG, logValue);
     }
 
     /**
@@ -63,6 +82,78 @@ public enum MyUtils {
     public static int px2dp(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 手机号验证
+     * @param phoneNumber
+     * @return
+     */
+    public static boolean dial(final String phoneNumber) {
+        Pattern p = Pattern.compile("^(1[3-9])\\d{9}$");
+        Matcher m = p.matcher(phoneNumber);
+        return m.matches();
+    }
+
+    public void setTimePickCallBack(TimePickCallBack timePickCallBack) {
+        this.timePickCallBack = timePickCallBack;
+    }
+
+    private TimePickCallBack timePickCallBack;
+    private Dialog mDialog;
+
+    public void initTimePicker(Context context, final TextView textView) {
+        Calendar startDate = Calendar.getInstance();
+        startDate.set(1949, 00, 01);//开始日期
+        TimePickerView pvTime = new TimePickerBuilder(context, new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(java.util.Date date, View v) {
+                String str = getTime(date);
+                textView.setText(str);
+                if (timePickCallBack != null) {
+                    timePickCallBack.back(str);
+                }
+            }
+        }).setType(new boolean[]{true, true, true, false, false, false})
+                .setCancelText("取消")//取消按钮文字
+                .setSubmitText("确定")
+                .setTitleText("选择时间")
+                .setLabel("", "", "", "", "", "")//默认设置为年月日时分秒
+                .setDate(Calendar.getInstance())
+                .setRangDate(startDate, Calendar.getInstance())
+                .isDialog(true) //默认设置false ，内部实现将DecorView 作为它的父控件。
+                .build();
+        Button btnSubmit = pvTime.getDialogContainerLayout().getRootView().findViewById(R.id.btnSubmit);
+        Button btnCancel = pvTime.getDialogContainerLayout().getRootView().findViewById(R.id.btnCancel);
+        btnSubmit.setBackground(null);
+        btnCancel.setBackground(null);
+        mDialog = pvTime.getDialog();
+        if (mDialog != null) {
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    Gravity.BOTTOM);
+            params.leftMargin = 0;
+            params.rightMargin = 0;
+            pvTime.getDialogContainerLayout().setLayoutParams(params);
+            Window dialogWindow = mDialog.getWindow();
+            if (dialogWindow != null) {
+                dialogWindow.setWindowAnimations(R.style.picker_view_slide_anim);//修改动画样式
+                dialogWindow.setGravity(Gravity.BOTTOM);//改成Bottom,底部显示
+            }
+        }
+    }
+
+    public String getTime(java.util.Date date) {//可根据需要自行截取数据显示		 HH:mm:ss
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        return format.format(date);
+    }
+
+    public MyUtils showDialog() {
+        if (mDialog != null) {
+            mDialog.show();
+        }
+        return this;
     }
 
 
